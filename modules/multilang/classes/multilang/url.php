5eb912bcdb20e93f33dc15231e580890dad2071d
chmiello <bartek@chmiello.pl>
2012-02-21 13:05:48 +0100

    Pierwsza pełna wersja modułu

---
+++ modules/multilang/classes/multilang/url.php
@@ -0,0 +1,40 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * URL helper class.
+ *
+ * @package    Kohana
+ * @category   Helpers
+ * @author     Kohana Team
+ * @copyright  (c) 2007-2011 Kohana Team
+ * @license    http://kohanaframework.org/license
+ */
+class Multilang_URL extends Kohana_URL {
+	/**
+	 * Fetches an absolute site URL based on a URI segment.
+	 *
+	 *     echo URL::site('foo/bar');
+	 *
+	 * @param   string  $uri        Site URI to convert
+	 * @param   mixed   $protocol   Protocol string or [Request] class to use protocol from
+	 * @param   boolean $index		Include the index_page in the URL
+	 * @return  string
+	 * @uses    URL::base
+	 */
+	public static function site($uri = '', $protocol = NULL, $index = TRUE)
+	{
+		// Chop off possible scheme, host, port, user and pass parts
+		$path = preg_replace('~^[-a-z0-9+.]++://[^/]++/?~', '', trim($uri, '/'));
+
+		if ( ! UTF8::is_ascii($path))
+		{
+			// Encode all non-ASCII characters, as per RFC 1738
+			$path = preg_replace('~([^/]+)~e', 'rawurlencode("$1")', $path);
+		}
+
+		// Concat the URL
+		$url = URL::base($protocol, $index).$path;
+		if(!Multilang::factory() -> is_language($url)) { $url .= '/'.Multilang::factory() -> language(); }
+		return $url;
+	}
+
+} // End url
