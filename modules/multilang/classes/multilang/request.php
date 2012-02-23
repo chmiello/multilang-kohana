0a65b683dc98abc91d707b23e99e68bfdb356cae
chmiello <bartek@chmiello.pl>
2012-02-23 21:37:53 +0100

    poprawa bledu z parametrami

---
+++ modules/multilang/classes/multilang/request.php
@@ -0,0 +1,50 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+
+class Multilang_Request extends Kohana_Request {
+
+	
+	public static function process_uri($uri, $routes = NULL)
+	{
+		// Load routes
+		$routes = (empty($routes)) ? Route::all() : $routes;
+		$params = NULL;
+
+		foreach ($routes as $name => $route)
+		{
+			// We found something suitable
+			if ($params = $route->matches($uri))
+			{
+				
+				if ( ! empty($params['lang']))
+					return array(
+						'params' => $params,
+						'route' => $route,
+					);
+				else
+				{
+					// poszukiwaie języka
+					foreach($params as $key => $value)
+					{
+						if (Multilang::factory()->acceptLangs($value) and $key != 'lang')
+						{
+							$lang = $value;
+							break;
+						}
+					}
+					$params = $route->matches(str_replace('/'.$lang,'',$uri));
+					$params['lang'] = $lang;
+					return array(
+						'params' => $params,
+						'route' => $route,
+					);
+					
+					
+				}
+			}
+		}
+
+		return NULL;
+	}
+	
+} // End Request
---
+++ modules/multilang/classes/request.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+
+class Request extends Multilang_Request {
+} // End Request
