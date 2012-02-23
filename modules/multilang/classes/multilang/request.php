13f385578395bed729b8561be2ca07ca045e931e
chmiello <bartek@chmiello.pl>
2012-02-23 22:45:38 +0100

    poprawa bledu z parametrami nr 3

--- modules/multilang/classes/multilang/request.php
+++ modules/multilang/classes/multilang/request.php
@@ -9,14 +9,14 @@ class Multilang_Request extends Kohana_Request {
 		// Load routes
 		$routes = (empty($routes)) ? Route::all() : $routes;
 		$params = NULL;
-
+		//echo(Multilang::factory()->acceptLangs($uri));
 		foreach ($routes as $name => $route)
 		{
 			// We found something suitable
 			if ($params = $route->matches($uri))
 			{
 				
-				if ( ! empty($params['lang']))
+				if ( ! empty($params['lang']) or Multilang::factory()->acceptLangs($uri) )
 					return array(
 						'params' => $params,
 						'route' => $route,
@@ -24,19 +24,19 @@ class Multilang_Request extends Kohana_Request {
 				else
 				{
 					// poszukiwaie języka
-					//$lang = '';
 					foreach($params as $key => $value)
 					{
-						if (Multilang::factory()->acceptLangs($value) and $key != 'lang')
+						if (Multilang::factory()->acceptLangs($value) and ($key != 'lang'))
 						{
 							$lang = $value;
 							break;
 						}
 					}
-					if( ! isset($lang))
-					$lang = Multilang::factory()->default_lang();
-					$params = $route->matches(str_replace('/'.$lang,'',$uri));
-					$params['lang'] = $lang;
+					if ( ! empty($lang))
+					{	
+						$params = $route->matches(str_replace(''.$lang,'',$uri));
+						$params['lang'] = $lang;
+					}
 					return array(
 						'params' => $params,
 						'route' => $route,
--- modules/multilang/init.php
+++ modules/multilang/init.php
@@ -1,14 +1,7 @@
 <?php
-/**
- * The default route
- * It's a bit tricky and particular since it got no translations.
- * We need to create a general route for this one
- *
- * It is recommended to move this route into your bootstrap and adapt it.
- */
 
 
-Route::set('only.lang','', array('lang' => '('.implode('|',Multilang::factory() -> langs()).')'))
+Route::set('only.lang','', array('lang' => '('.implode('|',Multilang::factory()->langs()).')'))
 	-> defaults(array(
 		'direction' => 'default',
 		'controller' => 'index',
@@ -18,6 +11,6 @@ Route::set('only.lang','', array('lang' => '('.implode('|',Multilang::factory() 
 function __($string, array $values = NULL)
 {
 	// wyświetlanie ciągu w danym języku
-	$string = I18n::get($string,Multilang::factory() -> language());
+	$string = I18n::get($string,Multilang::factory()->language());
 	return empty($values) ? $string : strtr($string, $values);
 }
